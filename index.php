bd3b5953551144734968b07fd3bb0e37a7695fcf
mistymornin1878 <aldiansyah1878@gmail.com>
2021-02-10 19:38:07 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe quaerat et, at placeat fugiat vel, itaque dolorem rerum nihil, quo aspernatur corporis ratione enim voluptate similique dicta! Distinctio, magni delectus.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
