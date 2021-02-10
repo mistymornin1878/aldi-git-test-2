9f87011220d5977e30277b1193890882f5bc94f8
mistymornin1878 <aldiansyah1878@gmail.com>
2021-02-10 19:28:11 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe quaerat et, at placeat fugiat vel, itaque dolorem rerum nihil, quo aspernatur corporis ratione enim voluptate similique dicta! Distinctio, magni delectus.</p>
+</body>
+</html>
